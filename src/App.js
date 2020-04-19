903d56adf504899014412d81f8e141af336906ef
Kriyo <krikenn@gmail.com>
2020-04-19 17:49:19 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React, { Component, Fragment } from "react";
+
+export default class App extends Component {
+  render() {
+    return (
+      <Fragment>
+        <h1>Something</h1>
+      </Fragment>
+    );
+  }
+}
