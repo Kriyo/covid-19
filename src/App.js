40b421b7ce7bca6598421fc80cfe70ae1e5eda77
Kriyo <krikenn@gmail.com>
2020-04-19 22:27:57 +0100

    added api req

--- src/App.js
+++ src/App.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
+import { fetchData } from "./api";
+
 export default class App extends Component {
+  async componentDidMount() {
+    const data = await fetchData();
+    console.log(data);
+  }
+
   render() {
     return (
       <div className={styles.container}>
